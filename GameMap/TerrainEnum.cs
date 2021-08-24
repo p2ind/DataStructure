74840748458f86fe62b4ae154eca68f5b6bc863e
Pankaj Saxena <pankaj.saxena@gmail.com>
2021-08-24 22:16:50 +0530

    Array Example GameMap Console project

---
+++ GameMap/TerrainEnum.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameMap
+{
+    public enum TerrainEnum
+    {
+        GRASS,
+        SAND,
+        WATER,
+        WALL
+    }
+}
