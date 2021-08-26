bf5efb417a1a86dd242710ff9df4c4dedbb6dd93
Pankaj Saxena <pankaj.saxena@gmail.com>
2021-08-26 08:38:43 +0530

    Adding SelectionSort & YearlyTransportPlan project

---
+++ SelectionSorr/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelectionSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] ArrayValues = new int[9] { -11, 12, -42, 0, 1, 90, 68, 6, -9 };
+            SelectSort.Sort<int>(ArrayValues);
+            Console.WriteLine(string.Join(" | ", ArrayValues));
+            Console.ReadKey();
+
+
+            string s = "";
+        }
+    }
+}
---
+++ SelectionSorr/SelectSort.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelectionSort
+{
+    public static class SelectSort
+    {
+        public static void Sort<T>(T[] array) where T : IComparable
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                int minIndex = i;
+                T minValue = array[i];
+                for (int j = i+1; j < array.Length; j++)
+                {
+                    if(array[j].CompareTo(minValue) < 0)
+                    {
+                        minIndex = j;
+                        minValue = array[j];
+                    }
+                }
+                Swap<T>(array,i, minIndex);
+            }
+        }
+
+        private static void Swap<T>(T[] array, int first, int second)
+        {
+            T temp = array[first];
+            array[first] = array[second];
+            array[second] = temp;
+        }
+    }
+}
